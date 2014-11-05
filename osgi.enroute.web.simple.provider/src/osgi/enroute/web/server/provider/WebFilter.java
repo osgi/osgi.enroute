cc73470fa3c23ae3a2c3e54c6bf7af8eb7671a56
Peter Kriens <peter.kriens@aqute.biz>
2014-11-05 17:09:10 +0100

    wip

--- osgi.enroute.web.simple.provider/src/osgi/enroute/web/server/provider/WebFilter.java
+++ osgi.enroute.web.simple.provider/src/osgi/enroute/web/server/provider/WebFilter.java
@@ -19,21 +19,22 @@ import org.osgi.service.coordinator.Coordinator;
  */
 public class WebFilter implements Filter {
 
-	int					maxConnections;
-	String				maxConnectionMessage;
-	AtomicInteger		counter = new AtomicInteger(1000);
-	AtomicInteger		active	= new AtomicInteger();
-	private Coordinator	coordinator;
+	int maxConnections;
+	String maxConnectionMessage;
+	AtomicInteger counter = new AtomicInteger(1000);
+	AtomicInteger active = new AtomicInteger();
+	private Coordinator coordinator;
 
-	public WebFilter(int maxConnections, String maxConnectionMessage, Coordinator coordinator) {
+	public WebFilter(int maxConnections, String maxConnectionMessage,
+			Coordinator coordinator) {
 		this.maxConnections = maxConnections;
 		this.maxConnectionMessage = maxConnectionMessage;
 		this.coordinator = coordinator;
 	}
 
 	@Override
-	public void doFilter(ServletRequest req, ServletResponse rsp, FilterChain chain) throws IOException,
-			ServletException {
+	public void doFilter(ServletRequest req, ServletResponse rsp,
+			FilterChain chain) throws IOException, ServletException {
 
 		try {
 			HttpServletResponse hrsp = (HttpServletResponse) rsp;
@@ -43,22 +44,23 @@ public class WebFilter implements Filter {
 					String msg = maxConnectionMessage;
 					if (msg == null)
 						msg = "Too many simultaneous requests (this is a small server, after all, this is not a commercial service)";
-					hrsp.sendError(HttpServletResponse.SC_SERVICE_UNAVAILABLE, msg);
+					hrsp.sendError(HttpServletResponse.SC_SERVICE_UNAVAILABLE,
+							msg);
 					return;
 				}
 			}
 
-			Coordination c = coordinator.begin("osgi.enroute.webrequest."+active.incrementAndGet(), 0);
+			Coordination c = coordinator.begin("osgi.enroute.webrequest."
+					+ active.incrementAndGet(), 0);
 			try {
 				chain.doFilter(req, rsp);
-				c.end();
-			}
-			catch (Throwable t) {
+			} catch (Throwable t) {
 				c.fail(t);
 				throw t;
+			} finally {
+				c.end();
 			}
-		}
-		finally {
+		} finally {
 			active.decrementAndGet();
 		}
 	}
