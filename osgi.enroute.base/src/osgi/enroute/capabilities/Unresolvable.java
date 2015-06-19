7246f27ac3fa9949050783b2d9943a085edaeee2
BJ Hargrave <bj@bjhargrave.com>
2015-06-19 12:16:49 -0400

    unresolvable: Use a better namespace and a truly unsatisfiable filter

--- osgi.enroute.base/src/osgi/enroute/capabilities/Unresolvable.java
+++ osgi.enroute.base/src/osgi/enroute/capabilities/Unresolvable.java
@@ -6,7 +6,7 @@ import aQute.bnd.annotation.headers.RequireCapability;
  * The purpose of this capability is to be never satisfied. This can be required
  * by bundles that are compile only or class path only.
  */
-@RequireCapability(ns = "osgi.missing", filter = "(&(missing=true)(missing=false))")
+@RequireCapability(ns = "osgi.unresolvable", filter = "(&(must.not.resolve=*)(!(must.not.resolve=*)))")
 public @interface Unresolvable {
 
 }
