0e314b39d5692f6846bb6cc136878895af517844
BJ Hargrave <bj@bjhargrave.com>
2015-06-24 12:20:34 -0400

    obsolete: Delete obsolete package
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

--- osgi.enroute.base.api/src/osgi/enroute/scheduler/dto/ScheduleState.java
+++
@@ -1,5 +0,0 @@
-package osgi.enroute.scheduler.dto;
-
-public enum ScheduleState {
-	UNKNOWN, WAITING, RUNNING, SUCCESS, FAILED;
-}
--- osgi.enroute.base.api/src/osgi/enroute/scheduler/dto/ScheduleType.java
+++
@@ -1,5 +0,0 @@
-package osgi.enroute.scheduler.dto;
-
-public enum ScheduleType {
-	AFTER, AT, BEFORE, CRON, PERIOD, OTHER;
-}
