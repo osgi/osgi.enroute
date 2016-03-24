b3b97971afd90134997fd02ffd7d35544e8d7cbf
David Leangen <david.leangen@bioscene.co.jp>
2016-03-24 18:05:49 +0900

    Removed obsolete RootServlet and replaced with ConditionalServlet
    Signed-off-by: David Leangen <david.leangen@bioscene.co.jp>

---
+++ osgi.enroute.base.api/src/osgi/enroute/webserver/api/ConditionalServletConfig.java
@@ -0,0 +1,21 @@
+package osgi.enroute.webserver.api;
+
+/**
+ * Component properties for the {@link ConditionalServlet}.
+ */
+public @interface ConditionalServletConfig {
+	/**
+	 * Timeout of the blacklist, in milliseconds.
+	 * 
+	 * If a {@link ConditionalServlet} behaves badly and throws an Exception, it will
+	 * be blacklisted. Use this configuration property to configure the amount of time
+	 * that such servlets will be blacklisted.
+	 * 
+	 * A value of 0 means that nothing gets blacklisted. A value of -1 means that any
+	 * badly behaving servlet will be blacklisted "forever" (i.e for the entire lifecycle
+	 * of the provider.
+	 * 
+	 * Default value is 300000 (5 minutes).
+	 */
+	 long timeout() default 300000;
+}
