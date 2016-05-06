ebb2e9ad8032d5242f7079f93f6d0f1a6813c65a
Peter Kriens <peter.kriens@aqute.biz>
2016-05-06 17:51:12 +0200

    [log roller] Seems to work, not well tested and could use some more options

--- osgi.enroute.logreader.rolling.provider/src/osgi/enroute/logreader/rolling/provider/RollingImpl.java
+++ osgi.enroute.logreader.rolling.provider/src/osgi/enroute/logreader/rolling/provider/RollingImpl.java
@@ -5,6 +5,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.stream.Stream;
@@ -13,19 +15,28 @@ import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.log.LogEntry;
 import org.osgi.service.log.LogListener;
+import org.osgi.service.log.LogReaderService;
 import org.osgi.service.log.LogService;
+
+import osgi.enroute.debug.api.Debug;
 
 /**
  * 
  */
-@Component(name = "osgi.enroute.logreader.rolling")
+@Component(name = "osgi.enroute.logreader.rolling", immediate=true, property = {
+        Debug.COMMAND_SCOPE + "=rolling", Debug.COMMAND_FUNCTION + "=error"
+})
 public class RollingImpl extends Thread implements LogListener {
 	FileWriter	fw;
 	File		current;
 	File		root;
 
+	@Reference
+	LogService log;
+	
 	@interface Config {
 		String where() default "messages";
 
@@ -33,9 +44,9 @@ public class RollingImpl extends Thread implements LogListener {
 
 		int numberOfLogs() default 10;
 
-		int level() default LogService.LOG_WARNING;
+		int level() default LogService.LOG_DEBUG;
 
-		String format() default "%T %8s %s\n";
+		String format() default "%s %8s %s%n";
 	}
 
 	Config							config;
@@ -54,6 +65,7 @@ public class RollingImpl extends Thread implements LogListener {
 		if (!root.isDirectory())
 			throw new IllegalStateException("Cannot create directory " + root);
 
+		start();
 	}
 
 	@Deactivate
@@ -65,7 +77,7 @@ public class RollingImpl extends Thread implements LogListener {
 	
 	@Override
 	public void logged(LogEntry entry) {
-		if (entry.getLevel() < config.level())
+		if (entry.getLevel() > config.level())
 			return;
 
 		queue.offer(entry);
@@ -81,8 +93,8 @@ public class RollingImpl extends Thread implements LogListener {
 			while (!isInterrupted()) {
 
 				LogEntry entry = queue.take();
-				String log = String.format(config.format(), entry.getTime(), level(entry.getLevel()),
-						entry.getMessage(), entry.getException().getMessage(), entry.getBundle().getBundleId());
+				String log = String.format(config.format(), getFormattedDate(), level(entry.getLevel()),
+						entry.getMessage(), entry.getException(), entry.getBundle().getBundleId());
 
 				file.write(log.getBytes(StandardCharsets.UTF_8));
 
@@ -96,6 +108,7 @@ public class RollingImpl extends Thread implements LogListener {
 			return;
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
+			e.printStackTrace();
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e1) {
@@ -105,19 +118,25 @@ public class RollingImpl extends Thread implements LogListener {
 	}
 
 	private RandomAccessFile getNextFile() throws IOException {
-		String name = String.format("%T.log", System.currentTimeMillis());
-		File f = new File(root, name);
-		RandomAccessFile raf = new RandomAccessFile(f, "w");
+		String name = getFormattedDate();
+		File f = new File(root, name +".log");
+		RandomAccessFile raf = new RandomAccessFile(f, "rwd");
 		raf.seek(raf.length());
-		
+		raf.write("# next file\n".getBytes(StandardCharsets.UTF_8));
+		System.out.println("Rolling over to " + f);
 		purge();
 		
 		return raf;
 	}
 
+	private String getFormattedDate() {
+		String s = DateTimeFormatter.ISO_DATE_TIME.format(LocalDateTime.now());
+		return s;
+	}
+
 	private void purge() {
 		Stream.of(root.listFiles()) //
-				.sorted( (a,b) -> Long.compare(a.lastModified(), b.lastModified())) //
+				.sorted( (a,b) -> Long.compare(b.lastModified(), a.lastModified())) //
 				.skip(config.numberOfLogs()) //
 				.forEach( ff -> ff.delete());
 	}
@@ -137,4 +156,12 @@ public class RollingImpl extends Thread implements LogListener {
 		}
 	}
 
+	@Reference
+	void setLogReader(LogReaderService lsr) {
+		lsr.addLogListener(this);
+	}
+
+	public void error(String ... args) {
+		log.log(LogService.LOG_ERROR, Stream.of(args).reduce("", (a,b)-> a + " "+b));
+	}
 }
