14f9bd4ccf5d41ab142c979a15562917333cf2b0
Neil Bartlett <neil.bartlett@paremus.com>
2016-06-08 23:46:12 +0100

    WebResource bundle for webcomponentsjs
    Signed-off-by: Neil Bartlett <neil.bartlett@paremus.com>

---
+++ osgi.enroute.webcomponentsjs.webresource/src/osgi/enroute/webcomponentsjs/webresource/capabilities/WebcomponentsJSConstants.java
@@ -0,0 +1,9 @@
+package osgi.enroute.webcomponentsjs.webresource.capabilities;
+
+/**
+ * osgi.enroute.webcomponentsjs.webresource
+ */
+public interface WebcomponentsJSConstants {
+	String WEBCOMPONENTSJSPATH="/osgi/enroute/webcomponentsjs/webresource";
+	String WEBCOMPONENTSJSVERSION="0.7.2";
+}
