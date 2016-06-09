5162b2a850cd38387d00035c6d3c49b6e7ef4893
Neil Bartlett <neil.bartlett@paremus.com>
2016-06-09 19:50:21 +0100

    Added Polymer web resources and demo app
    Signed-off-by: Neil Bartlett <neil.bartlett@paremus.com>

---
+++ osgi.enroute.polymer.app.webresource/src/osgi/enroute/polymer/app/webresource/capabilities/PolymerAppConstants.java
@@ -0,0 +1,15 @@
+package osgi.enroute.polymer.app.webresource.capabilities;
+
+/**
+ * osgi.enroute.polymer.app.webresource
+ */
+public interface PolymerAppConstants {
+	/**
+	 * 
+	 */
+	String APPPATH="/osgi/enroute/polymer/app/webresource";
+	/**
+	 * 
+	 */
+	String APPVERSION="1.0.0";
+}
