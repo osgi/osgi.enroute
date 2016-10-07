ba3353447aaebac102d74a365511757a095c68b7
Peter Kriens <peter.kriens@aqute.biz>
2016-10-07 16:17:05 +0200

    Cleaned up repositories Moved API from web resources into this workspace because otherwise we have a circular reference

---
+++ osgi.enroute.base.api/src/osgi/enroute/google/angular/capabilities/AngularConstants.java
@@ -0,0 +1,15 @@
+package osgi.enroute.google.angular.capabilities;
+
+/**
+ */
+public interface AngularConstants {
+	
+	/**
+	 * 
+	 */
+	String ANGULAR_WEB_RESOURCE_NAME = "/google/angular";
+	/**
+	 * 
+	 */
+	String ANGULAR_WEB_RESOURCE_VERSION = "1.5.7";
+}
