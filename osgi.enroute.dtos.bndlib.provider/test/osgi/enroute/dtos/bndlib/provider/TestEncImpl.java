b74e2ee3c7606d11860d97165aaa03f5321082c8
mcurcija <mario.curcija@gmail.com>
2016-11-21 21:54:17 +0100

    completed "minimal" DTOs.Enc test
    sparated test for OutputStream and added pretty as well as ignoreNull (although noop)

--- osgi.enroute.dtos.bndlib.provider/test/osgi/enroute/dtos/bndlib/provider/TestEncImpl.java
+++ osgi.enroute.dtos.bndlib.provider/test/osgi/enroute/dtos/bndlib/provider/TestEncImpl.java
@@ -11,11 +11,13 @@ import junit.framework.TestCase;
 import osgi.enroute.dto.api.DTOs;
 
 /**
- * Basic {@link Enc} test case.
+ * Minimal {@link DTOs.Enc} test case.
  */
 public class TestEncImpl extends TestCase {
 
 	private static final String EXPECTED_ENCODED_CONTENT = "{\"id\":999,\"symbolicName\":\"com.example.test\"}";
+	private static final String EXPECTED_PRETTY_ENCODED_CONTENT = "{\n\t\"id\":999,\"symbolicName\":\"com.example.test\"\n}";
+
 	private static final BundleDTO bundleDTO = new BundleDTO();
 	static {
 		bundleDTO.id = 999;
@@ -28,22 +30,37 @@ public class TestEncImpl extends TestCase {
 		cut = new DTOsProvider().encoder(bundleDTO);
 	}
 
-	public void testAppendable() throws Exception {
+	public void testPut() throws Exception {
+		assertEquals(EXPECTED_ENCODED_CONTENT, cut.put());
+	}
+
+	public void testPutToAppendable() throws Exception {
 		try (Writer writer = new StringWriter()) {
 			cut.put(writer);
 			assertEquals(EXPECTED_ENCODED_CONTENT, writer.toString());
 		}
 	}
 
-	public void testOutputStream() throws Exception {
+	public void testPutToOutputStream() throws Exception {
 		try (OutputStream os = new ByteArrayOutputStream()) {
 			cut.put(os);
 			assertEquals(EXPECTED_ENCODED_CONTENT, os.toString());
 		}
+	}
+
+	public void testPutToOutputStreamWithEncoding() throws Exception {
 		try (OutputStream os = new ByteArrayOutputStream()) {
 			cut.put(os, "UTF-8");
 			assertEquals(EXPECTED_ENCODED_CONTENT, os.toString());
 		}
+	}
+
+	public void testPretty() throws Exception {
+		assertEquals(EXPECTED_PRETTY_ENCODED_CONTENT, cut.pretty().put());
+	}
+
+	public void testIgnoreNull() throws Exception {
+		assertEquals(EXPECTED_ENCODED_CONTENT, cut.ignoreNull().put());
 	}
 
 }
