1d1bb07549df35b6590f6f2b534b6c69bd0fe72f
Tim Ward <tim.ward@paremus.com>
2017-12-05 17:47:58 +0000

    Provide a basic project archetype
    This archetype sets up a top level project containing a simple DS component and an application export project
    
    Signed-off-by: Tim Ward <tim.ward@paremus.com>

---
+++ archetypes/project/src/main/resources/archetype-resources/__impl-artifactId__/src/main/java/ComponentImpl.java
@@ -0,0 +1,13 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import org.osgi.service.component.annotations.Component;
+
+@Component
+public class ComponentImpl {
+    
+    //TODO add an implementation
+    
+}
