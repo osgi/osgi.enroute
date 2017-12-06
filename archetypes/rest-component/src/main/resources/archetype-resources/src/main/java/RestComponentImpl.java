56c6d0b2cce54b8c9df9686b22f4c0a31103833e
Tim Ward <tim.ward@paremus.com>
2017-12-06 16:59:26 +0000

    Provide a basic JAS-RS archetype
    Signed-off-by: Tim Ward <tim.ward@paremus.com>

---
+++ archetypes/rest-component/src/main/resources/archetype-resources/src/main/java/RestComponentImpl.java
@@ -0,0 +1,24 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.jaxrs.whiteboard.propertytypes.JaxrsResource;
+
+@Component(service=RestComponentImpl.class)
+@JaxrsResource
+public class RestComponentImpl {
+    
+    //TODO add an implementation
+    
+    @Path("rest")
+    @GET
+    public String toUpper() {
+        return "Hello World!";
+    }
+    
+}
---
+++ archetypes/rest-component/src/test/resources/projects/basic/reference/impl-bundle/src/main/java/it/pkg/ComponentImpl.java
@@ -0,0 +1,10 @@
+package it.pkg;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component
+public class ComponentImpl {
+    
+    //TODO add an implementation
+    
+}
