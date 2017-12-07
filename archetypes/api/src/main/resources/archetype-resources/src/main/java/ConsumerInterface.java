5ef62b2bf5d36c5778fcffdf3ff1c50ed35653a1
Tim Ward <tim.ward@paremus.com>
2017-12-07 16:00:50 +0000

    Add an archetype for creating an API module
    Signed-off-by: Tim Ward <tim.ward@paremus.com>

---
+++ archetypes/api/src/main/resources/archetype-resources/src/main/java/ConsumerInterface.java
@@ -0,0 +1,18 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import org.osgi.annotation.versioning.ConsumerType;
+
+/**
+ * This is an interface which is designed to be implemented by users
+ * of this API rather than by implementations of the API. A good
+ * example of a consumer interface is <code>javax.servlet.Servlet</code>
+ */
+@ConsumerType
+public interface ConsumerInterface {
+    
+    //TODO add an API
+    
+}
---
+++ archetypes/api/src/main/resources/archetype-resources/src/main/java/package-info.java
@@ -0,0 +1,5 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+@org.osgi.annotation.versioning.Version("1.0.0")
+package ${package};
