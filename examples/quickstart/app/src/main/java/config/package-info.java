8c473a77207a502093df0fab40e5f425fd793f77
Tim Ward <tim.ward@paremus.com>
2018-10-23 16:58:58 +0200

    Updates to make enRoute palatable to Maven Central
    * Add JavaDoc and sources to application projects
    * Add signatures to all project artifacts
    * Add developer information to parent poms
    * Add a release profile so that release data is only produced when needed
    
    Signed-off-by: Tim Ward <tim.ward@paremus.com>
    (cherry picked from commit 9a37ad05536db72c5ec2cf2cc752d0ab6ac98313)

---
+++ examples/quickstart/app/src/main/java/config/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The quickstart example doesn't require any configuration.
+ * For more information about configuring applications take
+ * a look at the microservice example.
+ */
+package config;
