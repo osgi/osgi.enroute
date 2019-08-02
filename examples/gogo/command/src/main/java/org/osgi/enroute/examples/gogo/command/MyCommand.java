d57d569202a8361c84fceb3ef8a6ba65c5b1311f
Markus Rathgeb <maggu2810@gmail.com>
2019-08-02 08:49:29 +0200

    gogo: use upstream property type annotation
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

--- examples/gogo/command/src/main/java/org/osgi/enroute/examples/gogo/command/MyCommand.java
+++ examples/gogo/command/src/main/java/org/osgi/enroute/examples/gogo/command/MyCommand.java
@@ -1,12 +1,12 @@
 
 package org.osgi.enroute.examples.gogo.command;
 
-import org.osgi.enroute.examples.gogo.command.upstream.propertytypes.Command;
+import org.apache.felix.service.command.annotations.GogoCommand;
 import org.osgi.service.component.annotations.Component;
 
 @Component(service = {
         MyCommand.class } /* You need to provide the command as a service or it it will not be picked up. */)
-@Command(scope = "mycmd", function = { "foo", "bar" })
+@GogoCommand(scope = "mycmd", function = { "foo", "bar" })
 public class MyCommand {
     public static void foo() {
         System.out.println("my command foo");
